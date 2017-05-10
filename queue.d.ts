52b970ef4c4a194d9bcd62de7be4c4bfa6cab821
Aron Adler <aronadler@hotmail.com>
2017-05-10 12:32:48 +0100

    Add handmade declaration file ¯\_(ツ)_/¯

---
+++ queue.d.ts
@@ -0,0 +1,26 @@
+declare module "queue" {
+
+
+    declare type onComplete = (promResult: any, numFulfilled?: number, numRejected?: number) => void;
+    declare type asyncFunc = () => Promise<any>;
+    declare type taskHolder = Task[];
+    declare interface Task {
+        func: asyncFunc;
+        onFulfilled: onComplete;
+        onRejected: onComplete;
+        done: boolean;
+    }
+
+    declare interface QueueFactory {
+        new (concurrencyMax: number,
+            onFulfilled?: onComplete,
+            onRejected?: onComplete): Queue;
+    }
+
+
+    declare interface Queue {
+        push: (func: asyncFunc | asyncFunc[],
+            onFulfilled?: onComplete,
+            onRejected?: onComplete) => void
+    }
+}
