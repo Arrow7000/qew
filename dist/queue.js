adafd70c9152dc4ac1d4db5ca956477130c00b1b
Aron Adler <aron@smartowldesign.com>
2017-05-10 02:43:09 +0100

    Queue seems to be working as envisaged

--- dist/queue.js
+++ dist/queue.js
@@ -1,41 +1,92 @@
 "use strict";
 var Queue = (function () {
-    // private preExecute: asyncFunc[];
     function Queue(concurrencyMax, onFulfilled, onRejected) {
+        this.idCounter = 0;
+        this.tasks = [];
+        this.executing = [];
         this.max = concurrencyMax;
         this.onFulfilled = onFulfilled;
         this.onRejected = onRejected;
     }
-    Queue.prototype.push = function (func) {
-    };
-    Queue.prototype.execute = function (func) {
+    Queue.prototype.push = function (func, onFulfilled, onRejected) {
         var _this = this;
+        if (func.length) {
+            func.forEach(function (func) {
+                _this.addTask(func, onFulfilled, onRejected);
+            });
+        }
+        else {
+            this.addTask(func, onFulfilled, onRejected);
+        }
+    };
+    Queue.prototype.addTask = function (func, onFulfilled, onRejected) {
+        this.idCounter++;
+        var task = {
+            func: func,
+            onFulfilled: onFulfilled,
+            onRejected: onRejected,
+            done: false,
+            id: this.idCounter
+        };
+        this.tasks = this.tasks.concat([task]);
+        this.tryMove();
+    };
+    Queue.prototype.execute = function (task) {
+        var _this = this;
+        var func = task.func, onFulfilled = task.onFulfilled, onRejected = task.onRejected;
+        var fulfill = onFulfilled || this.onFulfilled;
+        var reject = onRejected || this.onRejected;
         func()
             .then(function (result) {
-            _this.onFulfilled(result);
+            fulfill(result);
             _this.numFulfilled++;
+            _this.doAfterEach(task);
         })
             .catch(function (error) {
-            _this.onRejected(error);
+            reject(error);
             _this.numRejected++;
+            _this.doAfterEach(task);
         });
+    };
+    Queue.prototype.doAfterEach = function (task) {
+        task.done = true;
+        this.executing = this.executing.filter(function (task) { return !task.done; });
+        this.tryMove();
+    };
+    Queue.prototype.move = function () {
+        if (this.executing.length >= this.max) {
+            throw new Error("Cannot add more than " + this.max + " tasks to execute simultaneously");
+        }
+        if (this.tasks.length < 1) {
+            throw new Error("No more tasks to load");
+        }
+        var len = this.tasks.length - 1; // get index of last task
+        var task = this.tasks[len]; // get last task
+        this.tasks = this.tasks.slice(0, len); // remove task from tasks list
+        this.executing = this.executing.concat([task]); // add task to executing list
+        this.execute(task);
+    };
+    Queue.prototype.tryMove = function () {
+        if (this.executing.length < this.max && this.tasks.length > 0) {
+            this.move();
+        }
     };
     return Queue;
 }());
 var queue = new Queue(2, console.log, console.error);
-queue.push(function () { return count(1000); });
-queue.push(function () { return count(1000); });
-queue.push(function () { return count(1000); });
-queue.push(function () { return count(1000); });
-queue.push(function () { return count(1000); });
-queue.push(function () { return count(1000); });
+var _loop_1 = function (i) {
+    queue.push(function () { return count(i, Math.random() * 2500); });
+};
+for (var i = 0; i < 10; i++) {
+    _loop_1(i);
+}
 // Main
 var counter = 0;
-function count(delay) {
+function count(number, delay) {
     return new Promise(function (resolve) {
         setTimeout(function () {
-            resolve(counter);
-            counter++;
+            resolve(number);
+            // counter++;
         }, delay);
     });
 }
--- src/queue.ts
+++ src/queue.ts
@@ -1,59 +1,142 @@
-
-
-
 type onComplete = (promResult: any, numFulfilled?: number, numRejected?: number) => void;
 type asyncFunc = () => Promise<any>;
+type taskHolder = task[];
 
-interface ObjectIdHolder {
-    [x: number]: asyncFunc;
+interface task {
+    func: asyncFunc;
+    onFulfilled: onComplete;
+    onRejected: onComplete;
+    done: boolean;
+    id: number;
 }
 
 
 class Queue {
+
+    /**
+     * Set by user
+     * Not intended to be mutable
+     */
     private max: number;
+    private delay: number;
     private onFulfilled: onComplete;
     private onRejected: onComplete;
+    private tasks: taskHolder;
+    private executing: taskHolder;
+
+    /**
+     * Internal state variables
+     */
+    private idCounter: number;
     private numFulfilled: number;
     private numRejected: number;
 
-    // private preExecute: asyncFunc[];
-
-
     constructor(
         concurrencyMax: number,
-        onFulfilled: onComplete,
-        onRejected: onComplete
+        onFulfilled?: onComplete,
+        onRejected?: onComplete
     ) {
+        this.idCounter = 0;
+        this.tasks = [];
+        this.executing = [];
+
         this.max = concurrencyMax;
         this.onFulfilled = onFulfilled;
         this.onRejected = onRejected;
     }
 
-    public push(func: asyncFunc): void {
+    public push(func: asyncFunc | asyncFunc[],
+        onFulfilled?: onComplete,
+        onRejected?: onComplete): void {
 
+        if (func.length) {
+            (<asyncFunc[]>func).forEach(func => {
+                this.addTask(func, onFulfilled, onRejected);
+            });
+        } else {
+            this.addTask((<asyncFunc>func), onFulfilled, onRejected);
+        }
     }
 
-    private execute(func: asyncFunc) {
+    private addTask(func: asyncFunc,
+        onFulfilled?: onComplete,
+        onRejected?: onComplete): void {
+
+        this.idCounter++;
+
+        const task: task = {
+            func,
+            onFulfilled,
+            onRejected,
+            done: false,
+            id: this.idCounter
+        };
+
+        this.tasks = [...this.tasks, task];
+
+        this.tryMove();
+    }
+
+    private execute(task: task) {
+        const { func, onFulfilled, onRejected } = task;
+        const fulfill = onFulfilled || this.onFulfilled;
+        const reject = onRejected || this.onRejected;
+
         func()
             .then(result => {
-                this.onFulfilled(result);
+                fulfill(result);
                 this.numFulfilled++;
+                this.doAfterEach(task);
             })
             .catch(error => {
-                this.onRejected(error);
+                reject(error);
                 this.numRejected++;
+                this.doAfterEach(task);
             });
+    }
+
+    private doAfterEach(task: task) {
+        task.done = true;
+        this.executing = this.executing.filter(task => !task.done);
+
+        this.tryMove();
+    }
+
+    private move() {
+        if (this.executing.length >= this.max) {
+            throw new Error(`Cannot add more than ${this.max} tasks to execute simultaneously`);
+        }
+        if (this.tasks.length < 1) {
+            throw new Error(`No more tasks to load`);
+        }
+        const len = this.tasks.length - 1; // get index of last task
+        const task = this.tasks[len]; // get last task
+        this.tasks = this.tasks.slice(0, len); // remove task from tasks list
+        this.executing = [...this.executing, task]; // add task to executing list
+        this.execute(task);
+    }
+
+    private tryMove() {
+        if (this.executing.length < this.max && this.tasks.length > 0) {
+            this.move();
+        }
     }
 }
 
 
 const queue = new Queue(2, console.log, console.error);
-queue.push(() => count(1000));
-queue.push(() => count(1000));
-queue.push(() => count(1000));
-queue.push(() => count(1000));
-queue.push(() => count(1000));
-queue.push(() => count(1000));
+
+for (let i = 0; i < 10; i++) {
+    queue.push(() => count(i, Math.random() * 2500));
+}
+
+
+// queue.push(() => count(1000));
+// queue.push(() => count(1000));
+// queue.push(() => count(1000));
+// queue.push(() => count(1000));
+// queue.push(() => count(1000));
+// queue.push(() => count(1000));
 
 
 
@@ -68,11 +151,11 @@ export = Queue;
 
 let counter = 0;
 
-function count(delay: number): Promise<number> {
+function count(number: number, delay: number): Promise<number> {
     return new Promise(resolve => {
         setTimeout(function () {
-            resolve(counter);
-            counter++;
+            resolve(number);
+            // counter++;
         }, delay);
     });
 }
