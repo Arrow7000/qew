e98967ecf8471d22cc535f1a4b09316b7d7751ae
Aron Adler <aronadler@hotmail.com>
2017-05-17 15:30:07 +0100

    Remove unneeded types and interfaces

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,3 @@
-//// <reference types="./src/basics.d.ts" />
-
-
 type callback = <T>(err: Error, result?: T) => void;
 type groupCallback = <T>(resultArray: GroupResult<T>[]) => void;
 type asyncFunc = <T>() => Promise<T>;
@@ -11,44 +8,14 @@ interface GroupResult<T> {
     error: Error;
 }
 
-interface GroupResultsHolder<T> {
-    [groupId: number]: (GroupResult<T>)[];
-}
-
-type resolve<T> = (result: T) => void;
-type reject = (error: Error) => void;
-
-
-/**
- * Tasks
- */
-interface TaskBasic {
-    func: asyncFunc;
-    done: boolean;
-    isGroupTask: boolean;
-}
-
-interface SingleTask extends TaskBasic {
-    callback: callback;
-}
-
-interface GroupTask extends TaskBasic {
-    groupCallback: groupCallback;
-    groupId: number;
-    index: number;
-}
-
-type taskHolder = (SingleTask | GroupTask[])[];
-
-
 declare class Qew {
     constructor(maxConcurrent: number = 1, delay: delay = 0);
 
-    public push(funcs: asyncFunc[], cb: groupCallback): this;
     public push(func: asyncFunc, cb: callback): this;
+    public push(funcs: asyncFunc[], cb: groupCallback): this;
 
-    public pushProm<T>(funcs: asyncFunc[]): Promise<(GroupResult<T>)[]>;
     public pushProm<T>(func: asyncFunc): Promise<T>;
+    public pushProm<T>(funcs: asyncFunc[]): Promise<(GroupResult<T>)[]>;
 }
 
 export = Qew;
