25d2fb99d527e1e5208cec788d05f1a50f98faa1
Aron Adler <aronadler@hotmail.com>
2017-07-03 15:53:29 +0100

    Add namespace

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,18 @@
+declare namespace qew {
+    interface GroupSuccessResult<T> {
+        result: T;
+    }
+    interface GroupFailResult {
+        error: Error;
+    }
+    type GroupResult<T> = GroupSuccessResult<T> | GroupFailResult;
+}
+
 type callback = <T>(err: Error, result?: T) => void;
-type groupCallback = <T>(resultArray: GroupResult<T>[]) => void;
+type groupCallback = <T>(resultArray: qew.GroupResult<T>[]) => void;
 type asyncFunc = <T>() => Promise<T>;
 type getNumber = () => number;
 type delay = number | getNumber;
-interface GroupSuccessResult<T> {
-    result: T;
-}
-interface GroupFailResult {
-    error: Error;
-}
-type GroupResult<T> = GroupSuccessResult<T> | GroupFailResult;
 
 
 declare class Qew {
@@ -19,9 +22,9 @@ declare class Qew {
     public push(funcs: asyncFunc[], cb: groupCallback): this;
 
     public pushProm<T>(func: () => Promise<T>): Promise<T>;
-    public pushProm<T>(funcs: (() => Promise<T>)[]): Promise<GroupResult<T>[]>;
+    public pushProm<T>(funcs: (() => Promise<T>)[]): Promise<qew.GroupResult<T>[]>;
 }
 
 export default Qew;
 
-export function isResolved<T>(result: GroupResult<T>): result is GroupSuccessResult<T>;
+export function isResolved<T>(result: qew.GroupResult<T>): result is qew.GroupSuccessResult<T>;
